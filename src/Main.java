c71a8a007a01f396a4fb807c8b1a62c44ee9c5c7
cihankurban <kurbancihan@gmail.com>
2019-12-05 09:15:39 +0300

    competitorApp

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,5 @@ public class Main {
         User user = UserUtil.requireInputsAndLogin(3, true);
         System.out.println("Hello, " + user.getUsername());
         MenuUtil.showMenuInfinite();
-
     }
 }
